744d470a3c56f98010cb21265e07a0074dcf8278
Ivan Monteiro <ivanmonteiro1607@gmail.com>
2025-11-05 11:00:28 -0300

    refactor: Altera os campos Service e Controller, de Ninjas E Missoes, para final

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 @Service
 public class NinjaService {
 
-    private NinjaRepository ninjaRepository;
+    private final NinjaRepository ninjaRepository;
 
     public NinjaService(NinjaRepository ninjaRepository) {
         this.ninjaRepository = ninjaRepository;
